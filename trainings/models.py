4190f01e6116dccfc196be8799935694cfd39327
NicolasMuras <nicolasmuras@gmail.com>
2021-10-02 13:32:12 -0300

    feat: training app deleted.

--- trainings/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class TrainingModule(models.Model):
-
-    name = models.CharField('Nombre', max_length=100, blank=True, null=True)
-    description = models.CharField('Descripción', max_length=500, blank=True, null=True)
